e521eac40e93f39403f733a5e5e64b08472d7d1a
Anugya <39938849+anugyas@users.noreply.github.com>
2022-04-16 14:11:42 -0400

    Add changes using Ethan's nb

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -8,35 +8,39 @@ from parlai.core.script import ParlaiScript, register_script
 from parlai.utils.strings import colorize
 import parlai.utils.logging as logging
 from parlai.core.torch_agent import Batch
+from parlai.utils.safety import OffensiveLanguageClassifier
 
-def run_classifier(agent, data_from_targetLM="This is a test message."):
-    # TODO: figure out how to create Batch object using parsed data from Target LM
-    harmfulness_score = agent.score(data_from_targetLM)
-    print("Harmfulness Score: ", harmfulness_score)
-    return harmfulness_score
+# def run_classifier(agent, data_from_targetLM="This is a test message."):
+#     # TODO: figure out how to create Batch object using parsed data from Target LM
+#     harmfulness_score = agent.score(data_from_targetLM)
+#     print("Harmfulness Score: ", harmfulness_score)
+#     return harmfulness_score
 
-@register_script("classifier", aliases=["classifier"])
-class Classifier(ParlaiScript):
-    @classmethod
-    def setup_args(cls):
-        parser = ParlaiParser(True, True)
-        parser.add_argument('-n', '-ne', '--num-examples', default=1)
-        print("Parser: ", parser)
-#         parser.set_defaults(interactive_mode=True)
-#         parser.set_defaults(print_scores=True)
-        parser.set_defaults(batch_size=1)
-        parser.set_defaults(task='bot_adversarial_dialogue')
-        parser.set_defaults(model_file='zoo:bot_adversarial_dialogue/multi_turn/model')
-        parser.set_defaults(datatype='valid')
-        return parser
+# @register_script("classifier", aliases=["classifier"])
+# class Classifier(ParlaiScript):
+#     @classmethod
+#     def setup_args(cls):
+#         parser = ParlaiParser(True, True)
+#         parser.add_argument('-n', '-ne', '--num-examples', default=1)
+#         print("Parser: ", parser)
+# #         parser.set_defaults(interactive_mode=True)
+# #         parser.set_defaults(print_scores=True)
+#         parser.set_defaults(batch_size=1)
+#         parser.set_defaults(task='bot_adversarial_dialogue')
+#         parser.set_defaults(model_file='zoo:bot_adversarial_dialogue/multi_turn/model')
+#         parser.set_defaults(datatype='valid')
+#         return parser
 
-    def run(self):
-        return create_agent(self.opt)
+#     def run(self):
+#         return create_agent(self.opt)
         
-        
-
-classifier_agent = Classifier.main()
-run_classifier(classifier_agent)
+# classifier_agent = Classifier.main()
+# run_classifier(classifier_agent)
 
 # for i in range(10):
 #     run_classifier(classifier_agent, data_from_targetLM="AAAAAAAAAAAAA"+str(i))
+
+clf_file = 'zoo:bot_adversarial_dialogue/multi_turn/model'
+offensiveness_classifier = OffensiveLanguageClassifier(custom_model_file = clf_file)
+# TODO: to be integrated w/ target lm
+# prob_offensiveness = offensiveness_classifier.contains_offensive_language(text_from_target_lm)
